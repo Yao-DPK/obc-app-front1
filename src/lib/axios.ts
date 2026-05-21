5521fa9afe9dc57205765f2861863b95ced33bd5
Konan Yao <yao.konan2709@gmail.com>
2026-05-21 05:04:12 +0200

    Added Register Route

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { useAuth } from 'src/hooks/useAuth';
 
 const api = await axios.create({
-  baseURL: 'dvcobc-app-back1.vercel.app',
+  baseURL: import.meta.env.VITE_API_URL || '',
   withCredentials: true,
 });
 
