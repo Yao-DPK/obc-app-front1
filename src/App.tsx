28b30dcfaac0b96c67268996b9aa7381b09e6410
Konan Yao <yao.konan2709@gmail.com>
2026-05-21 05:07:47 +0200

    Added Register Route

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import { BrowserRouter } from 'react-router-dom';
 import { AppRouter } from './AppRouter';
 import { useAuth } from './hooks/useAuth';
-import { useEffect } from 'react';
+//import { useEffect } from 'react';
 
 
 export default function App() {
-  const { restoreSession, isLoading } = useAuth();
+  const { isLoading } = useAuth();
 
-  useEffect(() => {
+  /* useEffect(() => {
     restoreSession();
   }, []);
-
+ */
   if (isLoading) {
     return <div className="flex items-center justify-center h-screen">Chargement...</div>;
   }
