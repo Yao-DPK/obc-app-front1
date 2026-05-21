b1925f3cf934a9275a208e3adb55cc1f50c378b7
Konan Yao <yao.konan2709@gmail.com>
2026-05-21 05:26:46 +0200

    Added Register Route

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { useAuth } from 'src/hooks/useAuth';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || '/api',
+  baseURL: import.meta.env.VITE_API_URL || '',
   withCredentials: true,
 });
 
