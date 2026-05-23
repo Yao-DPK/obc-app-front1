4f7a2325b5e769e8d3db5e99c633700944b210fd
Konan Yao <yao.konan2709@gmail.com>
2026-05-23 16:38:35 +0200

    Mise à jour App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter } from 'react-router-dom';
 import { AppRouter } from './AppRouter';
 import { useAuth } from './hooks/useAuth';
-import { useEffect } from 'react';
+//import { useEffect } from 'react';
 
 
 export default function App() {
