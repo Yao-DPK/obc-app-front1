63f4418b498649f0a9b4e55016ef371bd05aa705
Konan Yao <yao.konan2709@gmail.com>
2026-05-25 01:22:44 +0200

    Mise à jour Login.tsx

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,7 +1,6 @@
 // apps/web/src/pages/Register.tsx
 import { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
-import axios from 'axios';
 import { Button } from '../components/ui/button';
 import { Card, CardContent, CardHeader, CardTitle } from '../components/ui/card';
 import { Input } from '../components/ui/input';
