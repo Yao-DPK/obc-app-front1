84a6bf01f6a24b8022df53252f42d6de847a8597
Konan Yao <yao.konan2709@gmail.com>
2026-05-24 21:41:18 +0200

    Changed backend end Env url

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: process.env.VITE_API_URL,
+        target: process.env.VITE_API_URL || 'http://localhost:3031',
         changeOrigin: true,
       },
 
