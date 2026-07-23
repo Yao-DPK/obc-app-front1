9e04817a99a93fe7b63e25834d8c9e589edd74b7
Konan Yao <yao.konan2709@gmail.com>
2026-07-23 02:53:33 +0200

    Test

--- src/hooks/useInscriptionForm.ts
+++ src/hooks/useInscriptionForm.ts
@@ -66,6 +66,9 @@ export function useInscriptionForm() {
     const payload = {
       step1: step1Data,
       step2: step2Data,
+      step3: {
+        documents: requiredFiles.map((f) => ({ fileType: f.fileType, isObligatory: f.isObligatory })),
+      }, 
     };
     console.log(`1`);
     const formData = new FormData();
