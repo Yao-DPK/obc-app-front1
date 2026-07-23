c4c12a5664b72164bdbe4d3e4d0660b32d88b717
Konan Yao <yao.konan2709@gmail.com>
2026-07-23 02:59:25 +0200

    Test

--- src/hooks/useInscriptionForm.ts
+++ src/hooks/useInscriptionForm.ts
@@ -74,13 +74,12 @@ export function useInscriptionForm() {
     const formData = new FormData();
     console.log(`2`);
     formData.append('data', JSON.stringify(payload));
-    const signatureFile = getValues('signatureFile');
-    if (signatureFile) formData.append('signature', signatureFile);
-    console.log(`Step 3 files: ${JSON.stringify(requiredFiles)}`);
     requiredFiles.forEach((f) => {
       console.log(`Ajout ${f.fileType.name}`);
       formData.append(f.fileType.name, f.file)
     });
+    const signatureFile = getValues('signatureFile');
+    if (signatureFile) formData.append('signature', signatureFile);
     console.log(`3`);
     setIsSubmitting(true);
     try {
